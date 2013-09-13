1de116133120948f7a48f505516a14019ce8865b
Renzo Bianchi <rbianchi66@gmail.com>
2013-09-13 17:39:22 +0200

    Editing pannello edit question.

--- src/qeditquestion.py
+++ src/qeditquestion.py
@@ -4,72 +4,105 @@
 from PyQt4 import QtGui, Qt, QtCore
 from translation import L
 
-class QQuestionGridButton(QtGui.QPushButton):
-    def __init__(self, question, **kwargs):
-        QtGui.QPushButton.__init__(self, **kwargs)
-        if question is not None:
-            l = question.name
-        else:
-            l = L("EMPTY")
-        l = "\n" + l + "\n"
-        self.setText(l)
-        font = QtGui.QFont("Arial", 14, QtGui.QFont.Normal)
-        font.setStyle(QtGui.QFont.StyleItalic)
-        self.setFont(font)
+class QEditRadioAnswer(QtGui.QRadioButton):
+    def __init__(self, answer, parent, **kwargs):
+        QtGui.QRadioButton.__init__(self, answer.value, parent, **kwargs)
+        self.answer = answer
 
-class QSurveyGrid(QtGui.QWidget):
-    def __init__(self, survey, **kwargs):
-        QtGui.QWidget.__init__(self, **kwargs)
-        self.survey = survey
-        self.layout = QtGui.QVBoxLayout(self)
-        title = QtGui.QLabel(self.survey.name)
+class QEditCheckboxAnswer(QtGui.QCheckBox):
+    def __init__(self, answer, parent, **kwargs):
+        QtGui.QCheckBox.__init__(self, answer.value, parent, **kwargs)
+        self.answer = answer
+
+class QEditOpenAnswer(QtGui.QWidget):
+    def __init__(self, answer, parent, **kwargs):
+        QtGui.QWidget.__init__(self, parent, **kwargs)
+        self.layout = QtGui.QHBoxLayout(self)
+        self.layout.addWidget(QtGui.QLabel(answer.value))
+        e = QtGui.QLineEdit()
+        e.setReadOnly(True)
+        self.layout.addWidget(e)
+        self.answer = answer
+        
+        
+class QQuestionEditFrame(QtGui.QFrame):
+    def __init__(self, question, rows = 5, **kwargs):
+        QtGui.QFrame.__init__(self, **kwargs)
+        self.question = question
+        self.rows = rows
+        self.menu = None
+        self.columns = len(question.answers)/rows
+        if len(question.answers)%rows > 0:
+            self.columns += 1 
+        self.layout = QtGui.QHBoxLayout(self)
+        title = QtGui.QLabel(self.question.name)
         font = QtGui.QFont("Arial", 16, QtGui.QFont.Bold)
         title.setFont(font)
         title.setAlignment(QtCore.Qt.AlignCenter)
         self.layout.addWidget(title)
+        line = QtGui.QFrame()
+        line.setFrameStyle(QtGui.QFrame.VLine | QtGui.QFrame.Plain)
+        self.layout.addWidget(line)
         grid = QtGui.QGridLayout()
-        qk = self.survey.questions.keys()
-        qk.sort()
-        c = 1
-        for x in xrange(3):
-            for y in xrange(3):
-                q = self.survey.questions.get(c, None)
-                f = QQuestionGridButton(q)
-                grid.addWidget(f, x, y)
+        ak = self.question.answers.keys()
+        ak.sort()
+        c = 0
+        r = 0
+        for ai in ak:
+            el = QtGui.QHBoxLayout()
+            answer = self.question.answers[ai]
+            f = QEditOpenAnswer(answer, self)
+            el.addWidget(f)
+            pb = QtGui.QPushButton("-")
+            pb.setMaximumWidth(40)
+            el.addWidget(pb)
+            grid.addLayout(el, r, c)
+            r += 1
+            if r >= self.rows:
                 c += 1
+                r = 0
         self.layout.addLayout(grid)
-            
+        self.setMouseTracking(True)
         
-
-class QSurveyGridShow(QtGui.QMainWindow):
-    def __init__(self, survey):
-        super(QSurveyGridShow, self).__init__()
-        sg = QSurveyGrid(survey)
+    def mousePressEvent(self, e):
+        if e.button() == QtCore.Qt.RightButton:
+            pos = self.mapToGlobal(e.pos())
+            self.menu = QtGui.QMenu(self)
+            self.menu.addAction("&Add")
+            self.menu.addAction("&Remove")
+            self.menu.exec_(pos)
+            self.menu = None
+        
+        
+            
+class QQuestionEditShow(QtGui.QMainWindow):
+    def __init__(self, question):
+        super(QQuestionEditShow, self).__init__()
+        sg = QQuestionEditFrame(question)
         self.setCentralWidget(sg)
-        self.setWindowTitle("Survey Grid")
+        self.setWindowTitle("Question Edit Frame")
         self.show()
 
 
 if __name__ == "__main__":
     import sys
     from translation import loadTranslation
-    from survey import Survey
     from question import Question
     from answer import Answer
-    s = Survey("Survey Grid Test")
-    q = s.add(Question(1,"Question1"))
+    
+    q = Question(2,"Question2")
     q.add(Answer(1,"answer1"))
     q.add(Answer(2,"answer2"))
-    
-    q = s.add(Question(2,"Question2"))
     q.add(Answer(3,"answer3"))
     q.add(Answer(4,"answer4"))
     q.add(Answer(5,"answer5"))
+    q.add(Answer(6,"answer6"))
+    q.add(Answer(7,"answer7"))
 
     loadTranslation("en")
     
     app = QtGui.QApplication(sys.argv)
-    survey_grid = QSurveyGridShow(s)
+    survey_grid = QQuestionEditShow(q)
     survey_grid.setGeometry(200, 100, 800, 600)
     sys.exit(app.exec_())
     
--- src/question.py
+++ src/question.py
@@ -12,3 +12,14 @@ class Question(object):
         self.answers[a.id] = a
         return a
         
+class RadioQuestion(object):
+    def __init__(self, id = 0, name = "New Question"):
+        super(RadioQuestion, self).__init__(id, name)
+
+class CheckboxQuestion(object):
+    def __init__(self, id = 0, name = "New Question"):
+        super(CheckboxQuestion, self).__init__(id, name)
+
+class OpenQuestion(object):
+    def __init__(self, id = 0, name = "New Question"):
+        super(OpenQuestion, self).__init__(id, name)
